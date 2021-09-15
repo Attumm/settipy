e96c4307b3f01a249e9a3e2ca50f4304011e6747
Attumm <bijman.m.m@gmail.com>
2021-09-15 21:11:59 +0200

    Bumped version v1.6.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
 
-    version='1.5.0',
+    version='1.6.0',
     py_modules=['settipy'],
     license='MIT',
 
